1d93bf6efb1afcee8f143de6965d8890a8090a93
siva <siva.velusamy.kgm@gmail.com>
2025-03-23 22:48:56 +0530

    version modified

--- 1on1bet-be/authservice/src/main/java/com/_on1bet/authservice/controller/RegisterController.java
+++ 1on1bet-be/authservice/src/main/java/com/_on1bet/authservice/controller/RegisterController.java
@@ -1,7 +1,5 @@
 package com._on1bet.authservice.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,23 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com._on1bet.authservice.service.RegisterService;
 
 import jakarta.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("auth/v1/register/")
+@Slf4j
 public class RegisterController {
 
-    private  final RegisterService registerService;
+    private final RegisterService registerService;
 
-    @Autowired
-    public RegisterController(RegisterService registerService)
-    {
+    public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
     }
 
     @PostMapping("verify/mobileno")
-    public String firstTimeMobileNoRegisterHandler(@RequestParam("mobileno")  @Nonnull Long mobileNo,
-     @RequestParam("countrycode") @Nonnull Integer countrycodeid) {
+    public String firstTimeMobileNoRegisterHandler(@RequestParam("mobileno") @Nonnull Long mobileNo,
+            @RequestParam("countrycode") @Nonnull Integer countrycodeid) {
+        log.info("First time mobile number registration handler starts");
+        // return registerService.
         return "Working fine!";
     }
-    
+
 }
