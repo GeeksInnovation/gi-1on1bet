3bfed08c107c2022c0f080dd484f45175bffd614
GeeksInnovation <143426606+GeeksInnovation@users.noreply.github.com>
2025-04-01 23:04:36 +0530

    Feat/profile save (#10)
    * test commit to main
    
    * dashboard service added
    
    * checkstype uncommented
    
    ---------
    
    Co-authored-by: siva <siva.velusamy.kgm@gmail.com>

---
+++ 1on1bet-be/dashboardservice/src/main/java/com/_on1bet/dashboardservice/DashboardserviceApplication.java
@@ -0,0 +1,13 @@
+package com._on1bet.dashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardserviceApplication.class, args);
+	}
+
+}
---
+++ 1on1bet-be/dashboardservice/src/test/java/com/_on1bet/dashboardservice/DashboardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com._on1bet.dashboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
